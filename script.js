869e767c54410d33fb3221fd6f83f56dac512bfc
Gavin56 <gavinobrien56@gmail.com>
2020-10-04 14:07:54 -0400

    Created the arrays necessary to pick characters from. Added an empty array to dynamically select and generate user-specified character options

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// Assignment Code
+var userCharacters = [];
+var lowerCaseLetters =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericalValues = ["1", "2", "3", "4", "5", "6", "7", , "8", "9", "0"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "="];
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,7 +13,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
